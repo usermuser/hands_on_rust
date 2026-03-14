2db9f67d3729fa98f499b0b821b27f19242e9e79
usermuser <job.khusainov@gmail.com>
2026-03-14 14:07:42 +0500

    init flappy

---
+++ flappy/src/main.rs
@@ -0,0 +1,16 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 10, "hello, Bracket Terminal!")
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State {});
+}
