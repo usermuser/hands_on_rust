af1aa92916535828331373fc24e2c8e319af06dd
usermuser <job.khusainov@gmail.com>
2026-03-15 23:06:35 +0500

    add gravity_and_move for Player

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -32,6 +32,17 @@ impl Player {
             to_cp437('@'),
         );
     }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
 }
 struct State {
     mode: GameMode,
