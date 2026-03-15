a5d3a56a23d78dee1635ec984e881b32c8c73d98
usermuser <job.khusainov@gmail.com>
2026-03-15 22:51:08 +0500

    add player

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -8,6 +8,31 @@ enum GameMode {
     End,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@'),
+        );
+    }
+}
 struct State {
     mode: GameMode,
 }
