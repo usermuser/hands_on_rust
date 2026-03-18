1258831c0acc82b162c77c7ee84d6ea12259bf8e
usermuser <job.khusainov@gmail.com>
2026-03-18 12:58:42 +0500

    init dungeon crawler

---
+++ dungeoncrawl/src/main.rs
@@ -0,0 +1,21 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl State {
+    fn new() -> Self {
+        State {}
+    }
+}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1, "Hello, Bracket Terminal!");
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("dungeoncrawl")
+        .build()?;
+    main_loop(context, State::new())
+}
