1455a4acbe94dc5bec182d2857f612b838985c29
usermuser <job.khusainov@gmail.com>
2026-03-26 00:46:18 +0500

    fix bug with moving

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -1,18 +1,18 @@
 use crate::prelude::*;
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
     Wall,
     Floor,
 }
 
-pub struct Map {
-    pub tiles: Vec<TileType>,
-}
-
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
 }
 
 impl Map {
@@ -21,68 +21,9 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(0);
-        for y in camera.top_y .. camera.bottom_y {
-            for x in camera.left_x .. camera.right_x {
-                if self.in_bounds(Point::new(x,y)) {
-                let idx = map_idx(x, y);
-                match self.tiles[idx] {
-                    TileType::Floor => {
-                        ctx.set(
-                            x - camera.left_x,
-                            y - camera.top_y,
-                            WHITE,
-                            BLACK,
-                            to_cp437('.')
-                        );
-                    }
-                    TileType::Wall => {
-                        ctx.set(
-                            x - camera.left_x,
-                            y - camera.top_y,
-                            WHITE,
-                            BLACK,
-                            to_cp437('#')
-                        );
-                    }
-                }
-            }
-            }
-        }
 
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                let idx = map_idx(x, y);
-                match self.tiles[idx] {
-                    TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
-                    }
-                    TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
-                    }
-                }
-            }
-        }
-    }
-
-    /// Проверяет, находится ли точка в пределах игровой карты.
-    ///
-    /// Координаты считаются допустимыми, если:
-    /// * x от 0 до SCREEN_WIDTH - 1
-    /// * y от 0 до SCREEN_HEIGHT - 1
-    ///
-    /// # Аргументы
-    /// * `point` - точка с координатами для проверки
-    ///
-    /// # Возвращает
-    /// `true` если точка внутри карты, `false` в противном случае
     pub fn in_bounds(&self, point: Point) -> bool {
         point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
-    }
-
-    pub fn can_enter_tile(&self, point: Point) -> bool {
-        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
 
     pub fn try_idx(&self, point: Point) -> Option<usize> {
@@ -90,6 +31,41 @@ impl Map {
             None
         } else {
             Some(map_idx(point.x, point.y))
+        }
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_idx(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('.'),
+                            );
+                        }
+                        TileType::Wall => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('#'),
+                            );
+                        }
+                    }
+                }
+            }
         }
     }
 }
